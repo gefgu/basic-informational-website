688e94fcb279631b9b10c1d2714ed27c332b90ad
gefgu <gefgu@hotmail.com>
2022-06-28 15:25:18 -0300

    Add server rendering of index page

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs = require("fs/promises");
 
 const hostname = "127.0.0.1";
 const port = 3000;
@@ -6,7 +7,9 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
-  res.end("ABC");
+  fs.readFile("./index.html", { encoding: "utf-8" }).then((data) =>
+    res.end(data)
+  );
 });
 
 server.listen(port, hostname, () => {
