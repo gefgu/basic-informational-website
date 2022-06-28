11c8dc2fc31bf14d7abf22596b64f59e183e3d60
gefgu <gefgu@hotmail.com>
2022-06-28 15:29:42 -0300

    Add server routing to all required pages

--- index.js
+++ index.js
@@ -5,12 +5,28 @@ const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
+  const url = req.url;
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
-  fs.readFile("./index.html", { encoding: "utf-8" }).then((data) =>
-    res.end(data)
-  );
+  if (url === "/") {
+    fs.readFile("./index.html", { encoding: "utf-8" }).then((data) =>
+      res.end(data)
+    );
+  }
+  if (url === "/about") {
+    fs.readFile("./about.html", { encoding: "utf-8" }).then((data) =>
+      res.end(data)
+    );
+  }
+  if (url === "/contact-me") {
+    fs.readFile("./contact-me.html", { encoding: "utf-8" }).then((data) =>
+      res.end(data)
+    );
+  } else {
+    fs.readFile("./404.html", { encoding: "utf-8" }).then((data) =>
+      res.end(data)
+    );
+  }
 });
 
 server.listen(port, hostname, () => {
