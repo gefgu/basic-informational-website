986b52caf92da3d9bf243cb2e129ba746b69b9db
gefgu <gefgu@hotmail.com>
2022-06-28 16:04:03 -0300

    Fix logic error

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
 const http = require("http");
 const fs = require("fs");
 
-const hostname = "127.0.0.1";
-const port = 8080;
-
 const server = http.createServer((req, res) => {
   const url = req.url;
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
   if (url === "/") {
-    fs.readFile("./index.html", { encoding: "utf-8" }, (err, data) =>
-      res.end(data)
-    );
-  }
-  if (url === "/about") {
-    fs.readFile("./about.html", { encoding: "utf-8" }, (err, data) =>
-      res.end(data)
-    );
-  }
-  if (url === "/contact-me") {
-    fs.readFile("./contact-me.html", { encoding: "utf-8" }, (err, data) =>
-      res.end(data)
-    );
+    try {
+      const data = fs.readFileSync("./index.html", { encoding: "utf-8" });
+      res.end(data);
+    } catch (err) {
+      console.log(err);
+    }
+  } else if (url === "/about") {
+    try {
+      const data = fs.readFileSync("./about.html", { encoding: "utf-8" });
+      res.end(data);
+    } catch (err) {
+      console.log(err);
+    }
+  } else if (url === "/contact-me") {
+    try {
+      const data = fs.readFileSync("./contact-me.html", { encoding: "utf-8" });
+      res.end(data);
+    } catch (err) {
+      console.log(err);
+    }
   } else {
-    fs.readFile("./404.html", { encoding: "utf-8" }, (err, data) =>
-      res.end(data)
-    );
+    try {
+      const data = fs.readFileSync("./404.html", { encoding: "utf-8" });
+      res.end(data);
+    } catch (err) {
+      console.log(err);
+    }
   }
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(8080, () => {
+  console.log(`Server running!`);
 });
