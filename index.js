e45292568cd02ad4320a4d8b6e68d193815ebba2
gefgu <gefgu@hotmail.com>
2022-06-28 15:27:18 -0300

    Add url logging

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  console.log(req.url);
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
   fs.readFile("./index.html", { encoding: "utf-8" }).then((data) =>
